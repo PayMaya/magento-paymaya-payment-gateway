a1cf6c634439330b648e83a5b335fa06c06ba8ef
Mico <mico@cynder.io>
2021-11-24 15:35:52 +0800

    feat: add production URL

--- Model/Config.php
+++ Model/Config.php
@@ -10,7 +10,7 @@ class Config
     protected $resourceConfig;
     protected $logger;
 
-    public static $moduleVersion = "1.0.0";
+    public static $moduleVersion = "1.0.1";
 
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
