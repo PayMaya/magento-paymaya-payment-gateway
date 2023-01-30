63f2e69ea67fe2c5e630a1bef5bb952545dd34e2
Micah Andrew Bule <mico@cynder.io>
2023-01-30 18:16:23 +0800

    chore: bump to version 1.1.0

--- Model/Config.php
+++ Model/Config.php
@@ -10,7 +10,7 @@ class Config
     protected $resourceConfig;
     protected $logger;
 
-    public static $moduleVersion = "1.0.5";
+    public static $moduleVersion = "1.1.0";
 
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
